afd8658ef35146b678a7a3f048c791fc0ceffa1a
Kevin Nguyen <me@kndwin.dev>
2022-10-04 12:13:38 +1100

    🔨: fixed types in SurveyTable

--- features/survey/components/SurveyTable.tsx
+++ features/survey/components/SurveyTable.tsx
@@ -39,10 +39,10 @@ const StyledOverflowContainer = tw.div<any>`overflow-auto rounded-xl
 	h-fit w-full border border-zinc-200
 	max-h-[calc(100vh-10em)] max-w-[calc(100vw-17em)] block
 `;
-const StyledTable = tw.table`bg-white table-auto border-collapse w-full`;
-const StyledRow = tw.tr``;
-const StyledCell = tw.td`p-4 text-sm`;
-const StyledTHead = tw.thead`bg-white border-b border-b-zinc-200 text-sm`;
-const StyledTBody = tw.tbody``;
-const StyledHeaderRow = tw(StyledRow)``;
-const StyledHeader = tw.th`p-4`;
+const StyledTable = tw.table<any>`bg-white table-auto border-collapse w-full`;
+const StyledRow = tw.tr<any>``;
+const StyledCell = tw.td<any>`p-4 text-sm`;
+const StyledTHead = tw.thead<any>`bg-white border-b border-b-zinc-200 text-sm`;
+const StyledTBody = tw.tbody<any>``;
+const StyledHeaderRow = tw(StyledRow)<any>``;
+const StyledHeader = tw.th<any>`p-4`;
